6b9a9530e5801094df03d696ca03394c41611d3f
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2024-01-24 04:18:19 +0500

    get data from github

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.get('/github-languages/:username', async (req, res) => {
+    const username = req.params.username;
+    const token = process.env.GITHUB_TOKEN;
+    const headers = { 'Authorization': `token ${token}` };
+
+    try {
+        const reposResponse = await axios.get(`https://api.github.com/users/${username}/repos`, { headers });
+        const repos = reposResponse.data;
+
+        const formattedData = await Promise.all(repos.map(async (repo) => {
+            const languagesResponse = await axios.get(repo.languages_url, { headers });
+            return {
+                name: repo.name,
+                stargazers_count: repo.stargazers_count,
+                forks_count: repo.forks_count,
+                main_language: repo.language || 'None',
+                all_languages: Object.keys(languagesResponse.data),
+                pushed_at: repo.pushed_at,
+                description: repo.description || 'No description',
+                html_url: repo.html_url,
+                license: repo.license ? repo.license.spdx_id : 'No license'
+            };
+        }));
+
+        res.json(formattedData);
+    } catch (error) {
+        res.status(500).send('Error fetching data from GitHub');
+    }
+});
+
+
+
+app.listen(port, host, () => {
+    console.log(`Server running on port http://${host}:${port}`);
+});
