2ae24b04cb95ce58c81135f83a01c300e4ef3462
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2024-01-24 08:35:14 +0500

    get releases data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CodeFolio
+
 CodeFolio is an innovative web-based resume that syncs with your GitHub account to display the latest information about your projects, skills, and achievements. Designed to showcase professional growth, CodeFolio keeps your portfolio up-to-date and dynamic.
+
+
+```
+npm install
+
+node server.js
+```
--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.get('/github-languages/:username', async (req, res) => {
 
         const formattedData = await Promise.all(repos.map(async (repo) => {
             const languagesResponse = await axios.get(repo.languages_url, { headers });
+
+            // Fetch releases data
+            const releasesResponse = await axios.get(`https://api.github.com/repos/${username}/${repo.name}/releases`, { headers });
+            const releases = releasesResponse.data.map(release => ({
+                published_at: release.published_at,
+                tag_name: release.tag_name,
+                html_url: release.html_url,
+            }));
+
             return {
                 name: repo.name,
                 stargazers_count: repo.stargazers_count,
@@ -27,7 +36,8 @@ app.get('/github-languages/:username', async (req, res) => {
                 pushed_at: repo.pushed_at,
                 description: repo.description || 'No description',
                 html_url: repo.html_url,
-                license: repo.license ? repo.license.spdx_id : 'No license'
+                license: repo.license ? repo.license.spdx_id : 'No license',
+                releases: releases, // Add releases data
             };
         }));
 
