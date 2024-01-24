e36a22183ea65e917bbdb658b011791b4f7604dd
Artracurs <0xdf00ff@gmail.com>
2024-01-24 17:55:02 +0500

    project list component

---
+++ frontend/src/Components/Projects/Header.tsx
@@ -0,0 +1,19 @@
+import s from './Header.module.scss'
+import React from 'react'
+
+type Props = {}
+
+export default function Header({ }: Props) {
+  return (
+    <div className={s.container}>
+      <div>
+        <p>Projects</p>
+        <p>count: {28}</p>
+        <p>main language: {'JavaScript'}</p>
+      </div>
+      <div className={s.tools_list}>
+
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/Components/ProjectsList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Projects/Header'
+
+type Props = {}
+
+export default function ProjectsList({}: Props) {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
